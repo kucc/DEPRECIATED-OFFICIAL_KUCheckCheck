cba9ae075cde9a2d63962cd7f43f44b7c19b3082
jayming66 <mjw8941@gmail.com>
2021-10-05 00:58:56 +0900

    SessionContainer

--- src/components/Main/SessionContainer/SessionContainer.jsx
+++ src/components/Main/SessionContainer/SessionContainer.jsx
@@ -2,20 +2,12 @@ import React from "react";
 import { useHistory } from "react-router";
 import * as S from "../style";
 
-function PSessionContainer({
-  title,
-  category,
-  Leader,
-  check,
-  id,
-  level,
-  credit,
-}) {
-  const history = useHistory();
+export default function SessionContainer(props) {
+const history = useHistory();
   return (
     <>
       <S.SessionContainer
-        onClick={() => history.push(`/detail/${category}/${id}`)}
+      //onClick={() => history.push(`/detail/${category}/${id}`)}
       >
         <S.SessionImg>
           {/* 이사이에 이미지 <img > 로 받아온 이미지 나중에 집어넣을 예정 */}
@@ -23,25 +15,25 @@ function PSessionContainer({
         <S.SessionExplainWrapper>
           <S.SessionText>
             <S.SessionTitle>
-              <p>{title}</p>
+              <p>{props.couseName}</p>
             </S.SessionTitle>
             <S.SessionExplain>
-              <p>{Leader} 세션장</p>
+              <p>{props.courseLeader} 세션장</p>
             </S.SessionExplain>
           </S.SessionText>
           <S.SessionLevel>
             <p>
-              난이도:{level} /{credit}학점
+              난이도:{props.difficulty} /{props.requireTime}학점
             </p>
           </S.SessionLevel>
-          {check ? (
+          {/* {props.check ? (
             <S.SessionFavorite>수강중</S.SessionFavorite>
           ) : (
             <S.SessionFavorite>신청하기</S.SessionFavorite>
-          )}
+          )} */}
         </S.SessionExplainWrapper>
       </S.SessionContainer>
     </>
   );
 }
-export default PSessionContainer;
+}
