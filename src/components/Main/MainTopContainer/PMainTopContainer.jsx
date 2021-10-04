af9f0b1a1ca9cde08b0acbf3ab509ef4470c2f14
LHJ <jjs01hwang@gmail.com>
2021-10-05 02:31:07 +0900

    edited Pmaintopcontainer

--- src/components/Main/MainTopContainer/PMainTopContainer.jsx
+++ src/components/Main/MainTopContainer/PMainTopContainer.jsx
@@ -5,21 +5,14 @@ import * as S from "../style";
 function PMainTopContainer() {
   return (
     <>
-      <S.MainTopWrapper
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr",
-          height: "30em",
-        }}
-      >
+      <S.MainTopWrapper>
         <div>
-          <S.MainTitle>
-            <p>
-              KUCC
-              <br />
-              길라잡이{" "}
-            </p>
-          </S.MainTitle>
+          <S.MainTitle1>
+            <p>KUCC</p>
+          </S.MainTitle1>
+          <S.MainTitle2>
+            <p>길라잡이 </p>
+          </S.MainTitle2>
           <S.MainExplain>
             <p>고려대학교 중앙 컴퓨터 동아리 세션관리시스템</p>
           </S.MainExplain>
@@ -28,7 +21,7 @@ function PMainTopContainer() {
         <div>
           <img
             style={{ width: "450px", marginTop: "-40px" }}
-            src="./img/KUCCicon.png"
+            src={"./img/KUCCicon.png"}
           />
         </div>
       </S.MainTopWrapper>
