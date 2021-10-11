f69b637cf7e249da39c47949f3b3f6f34f65baba
JungIna <ina80118@gmail.com>
2021-10-12 04:29:22 +0900

    ruledesign

---
+++ src/components/Rules/RulesContainer/style.jsx
@@ -0,0 +1,13 @@
+import {Collapse} from "antd";
+import styled from "styled-components";
+
+export const SCollapse = styled(Collapse)`
+    background-color: black;
+`;
+
+export const SPanel = styled(Collapse.Panel)`
+  .ant-collapse-header {
+    color: white!important;
+  }
+`;
+
