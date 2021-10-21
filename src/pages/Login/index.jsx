376a0a7a29a8ab3639e363df998bc59dad8f1740
Sejin Kim <sejjj120@gmail.com>
2021-10-21 17:33:57 +0900

    불필요한 훅 삭제

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import LoginForm from "./components/LoginForm";
 import Description from "../../components/Description";
 import { StyledRow, StyledCol, Wrapper } from "./style";
-import { useHistory } from "react-router";
 import DefaultLogo from "../../components/DefaultLogo";
 
 function Login() {
-  const history = useHistory();
-
   return (
     <StyledRow>
       <StyledCol span={6} offset={8}>
