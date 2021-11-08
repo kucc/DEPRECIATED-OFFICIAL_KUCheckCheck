e83b7a960841a038fea4032a82f7a6a39c1583c8
LHJ <jjs01hwang@gmail.com>
2021-11-08 21:25:07 +0900

    init

---
+++ src/pages/Courses/CourseAttendance/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CourseAttendanceTop from "./CourseAttendanceTop";
+
+function CourseAttendace() {
+  return (
+    <div>
+      <CourseAttendanceTop />
+      {/* map 함수로 render하기
+      <CourseAttendanceCard /> */}
+    </div>
+  );
+}
+
+export default CourseAttendace;
