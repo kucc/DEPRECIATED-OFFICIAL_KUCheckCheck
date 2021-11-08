ab4cbd4255ed5e703de24ec83a1c519de3dcc1d3
Sejin Kim <sejjj120@gmail.com>
2021-11-08 21:17:41 +0900

    잘못 삭제된 코스 등록 페이지 복구

---
+++ src/pages/SessionNew/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { SessionNewBox } from "../../components/SessionNew";
+
+function SessionNew() {
+  return (
+    <>
+      <SessionNewBox />
+    </>
+  );
+}
+export default SessionNew;
