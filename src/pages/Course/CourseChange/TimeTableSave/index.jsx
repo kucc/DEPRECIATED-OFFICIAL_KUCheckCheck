20e299ecf5c9c29ab2682cdf49eeff10dbfc3bf7
LHJ <jjs01hwang@gmail.com>
2021-12-20 01:34:16 +0900

    timetable에서 색상 선택 기능 추가

--- src/pages/Course/CourseChange/TimeTableSave/index.jsx
+++ src/pages/Course/CourseChange/TimeTableSave/index.jsx
@@ -5,6 +5,7 @@ import TimeTable from "../../../../components/TimeTable/TimeTable";
 function TimeTableSave({ courseInfo, courseId, timeTableInfo }) {
   const [selectedData, setselectedData] = useState([]);
   const [cellData, setcellData] = useState([]);
+  const [selectedColor, setselectedColor] = useState("#FE7773");
 
   const renderselectedData = (index, randomColor, timeHour, timeMin) => {
     return selectedData[index].slice(1).map((data, key) => {
@@ -26,41 +27,66 @@ function TimeTableSave({ courseInfo, courseId, timeTableInfo }) {
 
   const handleSubmit = () => {
     // 밝은 색 중 선택하게 변경
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     const courseNewTimeTable = {
-      time_9_00: renderselectedData(1, randomColor, 9, "00"),
-      time_9_30: renderselectedData(2, randomColor, 9, "30"),
-      time_10_00: renderselectedData(3, randomColor, 10, "00"),
-      time_10_30: renderselectedData(4, randomColor, 10, "30"),
-      time_11_00: renderselectedData(5, randomColor, 11, "00"),
-      time_11_30: renderselectedData(6, randomColor, 11, "30"),
-      time_12_00: renderselectedData(7, randomColor, 12, "00"),
-      time_12_30: renderselectedData(8, randomColor, 12, "30"),
-      time_13_00: renderselectedData(9, randomColor, 13, "00"),
-      time_13_30: renderselectedData(10, randomColor, 13, "30"),
-      time_14_00: renderselectedData(11, randomColor, 14, "00"),
-      time_14_30: renderselectedData(12, randomColor, 14, "30"),
-      time_15_00: renderselectedData(13, randomColor, 15, "00"),
-      time_15_30: renderselectedData(14, randomColor, 15, "30"),
-      time_16_00: renderselectedData(15, randomColor, 16, "00"),
-      time_16_30: renderselectedData(16, randomColor, 16, "30"),
-      time_17_00: renderselectedData(17, randomColor, 17, "00"),
-      time_17_30: renderselectedData(18, randomColor, 17, "30"),
-      time_18_00: renderselectedData(19, randomColor, 18, "00"),
-      time_18_30: renderselectedData(20, randomColor, 18, "30"),
-      time_19_00: renderselectedData(21, randomColor, 19, "00"),
-      time_19_30: renderselectedData(22, randomColor, 19, "30"),
-      time_20_00: renderselectedData(23, randomColor, 20, "00"),
-      time_20_30: renderselectedData(24, randomColor, 20, "30"),
-      time_21_00: renderselectedData(25, randomColor, 21, "00"),
-      time_21_30: renderselectedData(26, randomColor, 21, "30"),
-      time_22_00: renderselectedData(27, randomColor, 22, "00"),
-      time_22_30: renderselectedData(28, randomColor, 22, "30"),
+      time_9_00: renderselectedData(1, selectedColor, 9, "00"),
+      time_9_30: renderselectedData(2, selectedColor, 9, "30"),
+      time_10_00: renderselectedData(3, selectedColor, 10, "00"),
+      time_10_30: renderselectedData(4, selectedColor, 10, "30"),
+      time_11_00: renderselectedData(5, selectedColor, 11, "00"),
+      time_11_30: renderselectedData(6, selectedColor, 11, "30"),
+      time_12_00: renderselectedData(7, selectedColor, 12, "00"),
+      time_12_30: renderselectedData(8, selectedColor, 12, "30"),
+      time_13_00: renderselectedData(9, selectedColor, 13, "00"),
+      time_13_30: renderselectedData(10, selectedColor, 13, "30"),
+      time_14_00: renderselectedData(11, selectedColor, 14, "00"),
+      time_14_30: renderselectedData(12, selectedColor, 14, "30"),
+      time_15_00: renderselectedData(13, selectedColor, 15, "00"),
+      time_15_30: renderselectedData(14, selectedColor, 15, "30"),
+      time_16_00: renderselectedData(15, selectedColor, 16, "00"),
+      time_16_30: renderselectedData(16, selectedColor, 16, "30"),
+      time_17_00: renderselectedData(17, selectedColor, 17, "00"),
+      time_17_30: renderselectedData(18, selectedColor, 17, "30"),
+      time_18_00: renderselectedData(19, selectedColor, 18, "00"),
+      time_18_30: renderselectedData(20, selectedColor, 18, "30"),
+      time_19_00: renderselectedData(21, selectedColor, 19, "00"),
+      time_19_30: renderselectedData(22, selectedColor, 19, "30"),
+      time_20_00: renderselectedData(23, selectedColor, 20, "00"),
+      time_20_30: renderselectedData(24, selectedColor, 20, "30"),
+      time_21_00: renderselectedData(25, selectedColor, 21, "00"),
+      time_21_30: renderselectedData(26, selectedColor, 21, "30"),
+      time_22_00: renderselectedData(27, selectedColor, 22, "00"),
+      time_22_30: renderselectedData(28, selectedColor, 22, "30"),
     };
     timeTableInfo(courseNewTimeTable);
   };
+
+  const colorArray = [
+    "#FE7773",
+    "#D1DDDB",
+    "#85B8C8",
+    "#7FB174",
+    "#D3B8D8",
+    "#F2DD66",
+  ];
   return (
-    <div>
+    <div style={{ marginLeft: "20%", marginRight: "20%", marginTop: "20px" }}>
+      <div style={{ display: "flex", gap: "20px" }}>
+        {colorArray.map((color, key) => {
+          return (
+            <div
+              style={{
+                height: selectedColor === color ? "55px" : "50px",
+                width: selectedColor === color ? "55px" : "50px",
+                backgroundColor: color,
+                borderRadius: "50%",
+                cursor: "pointer",
+                zIndex: "0",
+              }}
+              onClick={() => setselectedColor(color)}
+            />
+          );
+        })}
+      </div>
       <TimeTable
         editable="true"
         selectedData={(selectedData) => setselectedData(selectedData)}
