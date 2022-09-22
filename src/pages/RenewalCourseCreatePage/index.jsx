917f33022f8216f5b977399ff091fc32a9f9fcfe
유영인 <asst2207051@tosspartners.com>
2022-09-22 15:21:57 +0900

    feat: 활동 개설 페이지 레이아웃

--- src/pages/RenewalCourseCreatePage/index.jsx
+++ src/pages/RenewalCourseCreatePage/index.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { StyledBody, StyledContainer, StyledButton, StyledContainerHeader, StyledContainerSubTitle, StyledContainerTitle, StyledHeader, StyledTitle } from './style';
 
 export const RenewalCourseCreatePage = () => {
+
   return (
-    <div>
-      활동개설
-      <span> 부탁해요 </span>
-    </div>
+    <StyledBody>
+      <StyledHeader>
+        <StyledTitle>활동 개설</StyledTitle>
+        <StyledButton>등록하기</StyledButton>
+      </StyledHeader>
+      <StyledContainer>
+        <StyledContainerHeader>
+          <StyledContainerTitle>
+            필수 정보
+          </StyledContainerTitle>
+          <StyledContainerSubTitle>
+            필수 정보는 <span className='warning'>수정이 불가</span>하니 신중히 입력해주세요!
+          </StyledContainerSubTitle>
+        </StyledContainerHeader>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </StyledContainer>
+      <StyledContainer>
+        <StyledContainerHeader>
+          <StyledContainerTitle>
+            세부 정보
+          </StyledContainerTitle>
+        </StyledContainerHeader>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </StyledContainer>
+      <StyledContainer>
+        <StyledContainerHeader>
+          <StyledContainerTitle>
+            커리큘럼
+          </StyledContainerTitle>
+        </StyledContainerHeader>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </StyledContainer>
+    </StyledBody>
   );
 };
+
