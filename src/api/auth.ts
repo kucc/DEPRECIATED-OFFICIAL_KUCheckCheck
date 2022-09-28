f1e7bd042945be82962791ed37d7e5a76ba58c6f
TAEWOONG KANG <77313674+Taewoong1378@users.noreply.github.com>
2022-09-28 16:01:24 +0900

    Fix: QueryKey 타입 재정의

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,9 @@
+import { QueryFunctionContext } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const authSignup = ({ queryKey }: { queryKey: string[] }) => {
+export const authSignup = ({
+  queryKey,
+}: QueryFunctionContext<[string, string, string, string, string]>) => {
   const [, email, password, name, comment] = queryKey;
 
   return axios.post('/auth/signup', {
