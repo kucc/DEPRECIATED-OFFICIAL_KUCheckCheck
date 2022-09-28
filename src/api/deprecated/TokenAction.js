292c0a84bfa4845219d122d8ee1e9d0387119beb
TAEWOONG KANG <77313674+Taewoong1378@users.noreply.github.com>
2022-09-28 11:12:47 +0900

    Fix: 이전 api 요청 (fireabse) deprecated로 이동시킴

---
+++ src/api/deprecated/TokenAction.js
@@ -0,0 +1,41 @@
+import jwt from 'jwt-decode';
+import moment from 'moment';
+
+const tokenKey = 'accessToken';
+
+export function getToken() {
+  return localStorage.getItem(tokenKey);
+}
+
+export function removeToken() {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getMember() {
+  const accessToken = getToken();
+
+  if (!accessToken) {
+    return null;
+  }
+
+  const member = jwt(accessToken);
+  const newDate = moment().toDate().getTime() / 1000;
+  member.isLoggedIn = member.exp > newDate;
+  return member;
+}
+
+export function isLoggedIn(exp) {
+  let expireAt = exp;
+  if (!expireAt) {
+    const member = getMember();
+    expireAt = member.exp;
+  }
+  const newDate = moment().toDate().getTime() / 1000;
+
+  if (expireAt < newDate) {
+    console.log('만료');
+    return false;
+  }
+
+  return true;
+}
