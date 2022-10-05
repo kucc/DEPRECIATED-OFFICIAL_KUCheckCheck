9f61680f29b4aa30c45028c90cdee6a8c055f9bb
유영인 <asst2207051@tosspartners.com>
2022-10-05 20:06:35 +0900

    feat: AuthInputWithLabel ts 마이그레이션, react-hook-form 사용할 수 있도록 prop 설정

--- src/components/RenewalInputs/AuthInputWithLabel/index.tsx
+++ src/components/RenewalInputs/AuthInputWithLabel/index.tsx
@@ -1,23 +1,34 @@
-import PropTypes from 'prop-types';
+import { UseFormRegister } from 'react-hook-form';
 
 import { StyledInput, StyledLabel, StyledRequiredText, Wrapper } from './style';
 
+interface Props {
+  inputName: string;
+  register: UseFormRegister<any>;
+  labelTitle?: string;
+  inputType?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: () => void;
+  isRequired?: boolean;
+}
+
 export const AuthInputWithLabel = ({
-  labelTitle,
+  labelTitle = '',
   inputName,
   inputType,
   placeholder,
   value,
   onChange,
   isRequired = true,
-}) => {
+  register,
+}: Props) => {
   return (
-    <Wrapper isLabelTitle={labelTitle}>
-      {labelTitle && (
-        <StyledLabel htmlFor={inputName}>{labelTitle}</StyledLabel>
-      )}
+    <Wrapper hasLabelTitle={labelTitle !== ''}>
+      {labelTitle && <StyledLabel htmlFor={inputName}>{labelTitle}</StyledLabel>}
       {!isRequired && <StyledRequiredText>(선택)</StyledRequiredText>}
       <StyledInput
+        {...register(inputName)}
         name={inputName}
         type={inputType}
         placeholder={placeholder}
@@ -26,14 +37,4 @@ export const AuthInputWithLabel = ({
       />
     </Wrapper>
   );
-};
-
-AuthInputWithLabel.propTypes = {
-  labelTitle: PropTypes.string,
-  inputName: PropTypes.string,
-  inputType: PropTypes.string,
-  placeholder: PropTypes.string,
-  value: PropTypes.string,
-  onChange: PropTypes.func,
-  isRequired: PropTypes.bool,
 };
