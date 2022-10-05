08666f681ac9e5058b43f58c40503804fb726d0b
유영인 <asst2207051@tosspartners.com>
2022-10-05 20:26:31 +0900

    feat: AuthTextAreaWithLabel ts 마이그레이션, react-hook-form 사용할 수 있도록 prop 설정

---
+++ src/components/RenewalInputs/AuthTextAreaWithLabel/index.tsx
@@ -0,0 +1,41 @@
+import { UseFormRegister } from 'react-hook-form';
+
+import {
+  StyledLabel,
+  StyledRequiredText,
+  StyledTextArea,
+  Wrapper,
+} from '../AuthInputWithLabel/style';
+
+interface Props {
+  inputName: string;
+  register: UseFormRegister<any>;
+  labelTitle?: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: () => void;
+  isRequired?: boolean;
+}
+export const AuthTextAreaWithLabel = ({
+  labelTitle = '',
+  inputName,
+  placeholder,
+  value,
+  onChange,
+  isRequired = true,
+  register,
+}: Props) => {
+  return (
+    <Wrapper hasLabelTitle={labelTitle !== ''}>
+      {labelTitle && <StyledLabel htmlFor={inputName}>{labelTitle}</StyledLabel>}
+      {!isRequired && <StyledRequiredText>(선택)</StyledRequiredText>}
+      <StyledTextArea
+        {...register(inputName)}
+        name={inputName}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </Wrapper>
+  );
+};
